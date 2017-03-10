e0fcadbfebe234ad3f6f49adf4b70c17798f2217
Hatem Ghazouani <hatem-g@live.com>
2017-03-10 12:43:30 +0100

    test paragraph lorem

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ $ferrari= new Voiture();
 <body>
 <div class="container">
     <h1>Hello Index</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut ducimus, eveniet illo nesciunt
+        perferendis praesentium repellendus ullam voluptates! Assumenda dolorum earum eos exercitationem quae quam quo
+        repellat reprehenderit ullam?</p>
 </div>
 </body>
 </html>
